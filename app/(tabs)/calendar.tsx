72b7788f3c9c4d19bbdc488b49c932f20217c8ac
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-19 22:33:09 +0300

    refactor(app): change calendar to timeline

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -3,11 +3,6 @@ import React from 'react'
 import ExamSchedule from '../../components/ExamSchedule'
 import exams from '../../constants/exams';
 
-const examData = [
-  { time: '09:00', title: 'Math Exam', description: 'Chapter 1-3', },
-  { time: '11:00', title: 'History Exam', description: 'Ancient Civilizations', },
-  // Add more exam data as needed
-];
 
 const calendar = () => {
   return (
