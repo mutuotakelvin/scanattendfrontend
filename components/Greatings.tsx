2270be0e7323478b6a519e91e40753327c773157
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-19 20:12:49 +0300

    feat(app): greatings component

---
+++ components/Greatings.tsx
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Ionicons } from '@expo/vector-icons';
+
+const Greatings = () => {
+  return (
+    <View className='m-2 rounded-lg bg-gray-400 p-4 flex flex-row justify-between items-center'>
+      <View className='w-[70%]'>
+        <Text className='text-2xl font-bold'>Hi, Kelvin</Text>
+        <Text>Stay organized and on top of your exams
+          with just a click. Best of luck!</Text>
+      </View>
+      <View className='rounded-full h-16 w-16 bg-slate-600 flex justify-center items-center'>
+      <Ionicons name="notifications-sharp" size={50} color="black" />
+      </View>
+    </View>
+  )
+}
+
+export default Greatings
