9ac3a12213e27e6d72724968f7f0ba8b9ce41f96
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-19 22:15:09 +0300

    refactor(app): change to a timeline instead

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,12 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Calendar from '../../components/Calendar'
+import ExamSchedule from '../../components/ExamSchedule'
+import exams from '../../constants/exams';
 
+const examData = [
+  { time: '09:00', title: 'Math Exam', description: 'Chapter 1-3', },
+  { time: '11:00', title: 'History Exam', description: 'Ancient Civilizations', },
+  // Add more exam data as needed
+];
 
 const calendar = () => {
   return (
-    <View>
-      <Calendar />
+    <View className='h-full'>
+      <ExamSchedule examData={exams}/>
     </View>
   )
 }
