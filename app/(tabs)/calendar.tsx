a615d43843a5c6189e9732e2da5495e99f876e16
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-19 20:05:52 +0300

    feat(app): calendar functionality

---
+++ app/(tabs)/calendar.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Calendar from '../../components/Calendar'
+
+
+const calendar = () => {
+  return (
+    <View>
+      <Calendar />
+    </View>
+  )
+}
+
+export default calendar
