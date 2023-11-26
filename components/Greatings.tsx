d2714aedbb883d8c73e1497202374e51f7dcef21
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-26 23:11:34 +0300

    feat(app): binding

--- components/Greatings.tsx
+++ components/Greatings.tsx
@@ -1,12 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
+import useUserStores from '../store/UserStore';
+
 
 const Greatings = () => {
+  const {user}:any = useUserStores()
+  const userData = typeof user === 'string' ? JSON.parse(user) : user;
   return (
     <View className='m-2 rounded-lg bg-gray-400 p-4 flex flex-row justify-between items-center'>
       <View className='w-[70%]'>
-        <Text className='text-2xl font-bold'>Hi, Kelvin</Text>
+        <Text className='text-2xl font-bold'>Hi, {userData.username}</Text>
         <Text>Stay organized and on top of your exams
           with just a click. Best of luck!</Text>
       </View>
