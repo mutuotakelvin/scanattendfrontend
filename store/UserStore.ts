ef792b7abc368d231efc92cec46dd8393425a51a
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-26 23:14:29 +0300

    refactor(app): code refactoring

---
+++ store/UserStore.ts
@@ -0,0 +1,68 @@
+import {create} from 'zustand'
+
+const useUserStore = create((set) => ({
+    user: {},
+    setUser: (user:any) => set((state:any) =>{
+        return{
+            user: user
+        }
+    }),
+    clearUser: () => set((state:any) => {
+        return{
+            user: {}
+        }
+    }),
+    updateStudentDetails: async( userData:any) => {
+        const {id,department,fullName,regNumber,school,number} = userData
+        
+        const response = await fetch(`http://10.0.2.2:8000/api/student/${id}/`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                student_id:regNumber,
+                name:fullName,
+                phone:number,
+                school:school,
+                department:department,
+            })
+        })
+        const data = await response.json()
+        if(data.status === 'success'){
+            set((state:any) => {
+                return{
+                    user: data.user
+                }
+            })
+        }
+    },
+    updateLecturerDetails: async( userData:any) => {
+        const {id,department,fullName,regNumber,school,number} = userData
+        
+        const response = await fetch(`http://10.0.2.2:8000/api/teacher/${id}/`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                student_id:regNumber,
+                name:fullName,
+                phone:number,
+                school:school,
+                department:department,
+            })
+        })
+        const data = await response.json()
+        if(data.status === 'success'){
+            set((state:any) => {
+                return{
+                    user: data.user
+                }
+            })
+        }
+    },
+
+}))
+
+export default useUserStore
