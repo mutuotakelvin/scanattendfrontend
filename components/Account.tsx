d04aaac44622b647a67ed41797223eb33e255a85
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-26 23:13:54 +0300

    refactor(app): code refactoring

---
+++ components/Account.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Account = (props:any) => {
+    const {user} = props
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default Account
