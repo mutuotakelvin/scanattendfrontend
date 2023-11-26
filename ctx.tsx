71947b9a0b040ef715bdbf898e01e3124a5085c3
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-26 23:14:05 +0300

    refactor(app): code refactoring

---
+++ ctx.tsx
@@ -0,0 +1,107 @@
+import React, { createContext, useContext } from 'react';
+import { useStorageState } from './useStorageState';
+import {  useRouter } from 'expo-router';
+import useUserStores from './store/UserStore';
+import useAccountStore from './store/AccountStore';
+
+interface AuthContextProps {
+  signIn: (loginData: { username: string; password: string }) => Promise<void>;
+  signOut: () => void;
+  session?: string | null;
+  isLoading: boolean;
+
+}
+
+const AuthContext = createContext<AuthContextProps | null>(null);
+
+export function useSession() {
+  const value = useContext(AuthContext);
+
+  if (!value) {
+    throw new Error('useSession must be wrapped in a <SessionProvider />');
+  }
+
+  return value;
+}
+
+export function SessionProvider(props: React.PropsWithChildren) {
+  const [[isLoading, session], setSession] = useStorageState('session');
+  const router = useRouter();
+  const {user, setUser, clearUser}:any = useUserStores()
+  const {account,} :any = useAccountStore()
+  const userData = typeof user === 'string' ? JSON.parse(user) : user;
+
+  const signIn = async (loginData: { username: string; password: string }) => {
+    try {
+      if(account == 'Student'){
+        const { username, password } = loginData;
+        const res = await fetch('http://10.0.2.2:8000/api/auth/student/login/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        });
+        if (!res.ok) {
+          throw new Error('Authentication failed');
+        }
+    
+        const data = await res.json();
+        setUser(JSON.stringify(data.user))
+        // JSON-encode values before storing
+        setSession(JSON.stringify(data.access));
+        if(userData.username == null || userData.username == ''){
+          router.push('/complete-profile')
+        }
+      } else if(account == 'Lecturer'){
+        const { username, password } = loginData;
+        const res = await fetch('http://10.0.2.2:8000/api/auth/teacher/login/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        });
+        if (!res.ok) {
+          throw new Error('Authentication failed');
+        }
+    
+        const data = await res.json();
+        setUser(JSON.stringify(data.user))
+        // JSON-encode values before storing
+        setSession(JSON.stringify(data.access));
+        if(userData.username == null || userData.username == ''){
+          router.push('/complete-profile')
+        }
+      }
+  
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+
+  const signOut = () => {
+    console.log('signing out');
+    setSession(null);
+    clearUser()
+    router.push('/')
+  };
+
+  const authContextValue: AuthContextProps = {
+    signIn,
+    signOut,
+    session,
+    isLoading,
+  };
+
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+function useUserStore(arg0: (state: any) => any) {
+  throw new Error('Function not implemented.');
+}
+
