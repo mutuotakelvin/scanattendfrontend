b5ccbe86f8585cb1350c67c92c6995b1c4b86bf6
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-26 23:05:15 +0300

    refactor(app): code refactoring

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,24 +1,58 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'expo-router';
+import {useSession, } from '../ctx';
+import useAccountStore from '../store/AccountStore';
+
 
 const signin = () => {
+    const {login, account}:any = useAccountStore()
     const router = useRouter()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const { session, isLoading, signIn} = useSession();
+
+
     const handleSignIn = () => {
         // Implement your sign-in logic here
+        const loginData = {
+            username,
+            password,
+        };
+        signIn(loginData).then((res) => {
+            console.log(res)
+        }
+        )
+        router.push('/(tabs)/')
       };
     
       return (
-        <View>
-          <Text>Sign In</Text>
-          <TextInput placeholder="Username" />
-          <TextInput placeholder="Password" secureTextEntry={true} />
-          <TouchableOpacity onPress={handleSignIn}>
-            <Text>Sign In</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push('/sign-up')}>
-            <Text>Create an account</Text>
-          </TouchableOpacity>
+        <View className='flex flex-col justify-center items-center my-auto'>
+          <Text className='text-2xl pb-'>Welcome To ScanAttend</Text>
+          <View className='flex flex-row items-center gap-1'>
+            <Text>Don't have an account? </Text>
+            <Text>{account}</Text>
+            <TouchableOpacity onPress={() => router.push('/select-account')}>
+              <Text>Create a new account</Text>
+            </TouchableOpacity>
+          </View>
+          <View className='flex flex-col items-center space-y-4 w-full mt-4'>
+            <TextInput 
+            className='border border-gray-500 w-[60%] px-1 focus:border-blue-400 rounded-lg' 
+            placeholder="Username" 
+            value={username}
+            onChangeText={(text) => setUsername(text)} />
+            <TextInput 
+            className='border border-gray-500 w-[60%] px-1 focus:border-blue-400 rounded-lg' 
+            placeholder="Password" secureTextEntry={true} 
+            value={password}
+            onChangeText={(text) => setPassword(text)}/>
+            <TouchableOpacity 
+            className='bg-blue-500 rounded-lg' onPress={handleSignIn}>
+              <Text className='text-white p-1 px-8 text-lg'>Sign In</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       );
 }
