8c394adb4402616c992e4721d1d6142eaec16bd1
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-27 14:46:26 +0300

    refactor(app): update index tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,11 +4,13 @@ import { useRouter } from 'expo-router'
 import Greatings from '../../components/Greatings'
 import ExamList from '../../components/ExamList'
 import MyList from '../../components/ExamList'
+import useAccountStore from '../../store/AccountStore'
 
 
 
 const index = () => {
   const router = useRouter()
+  const {account}:any = useAccountStore()
   return (
     <View>
       <View className='mt-6 h-[20%]'>
@@ -17,9 +19,13 @@ const index = () => {
           <Text className='text-xl font-medium'>
             Upcoming exams
           </Text>
-          <Pressable onPress={() => router.push('/(modals)/addExam')}>
-            <Text>Create exam session</Text>
-          </Pressable>
+          {
+            account == 'Lecturer' && (
+              <Pressable onPress={() => router.push('/(modals)/addExam')}>
+                <Text>Create exam session</Text>
+              </Pressable>
+            )
+          }
         </View>
       </View>
       <View className='m-1 h-[72%] mt-6 mb-4'>
