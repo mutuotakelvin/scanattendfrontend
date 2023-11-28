30dd8fa7cd118e5b2fd4b4f9788153f30a97cd7a
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-28 20:16:33 +0300

    feat(app): add exam feature

--- app/(modals)/addExam.tsx
+++ app/(modals)/addExam.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import {useSession} from './../../ctx'
+import useUserStore from '../../store/UserStore';
 
 const AddExam = () => {
   const [examName, setExamName] = useState('');
@@ -8,10 +10,19 @@ const AddExam = () => {
   const [examDate, setExamDate] = useState(new Date());
   const [examTime, setExamTime] = useState(new Date());
   const [examDuration, setExamDuration] = useState('');
+  const [course, setCourse] = useState('');
+  const [alert, setAlert] = useState({
+    isVisible: false,
+    msg:''
+})
+
+  const {session} = useSession()
+  const {user}:any = useUserStore()
 
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showTimePicker, setShowTimePicker] = useState(false);
 
+  const userData = typeof user === 'string' ? JSON.parse(user) : user;
   const handleDateChange = (event:any, selectedDate:any) => {
     setShowDatePicker(Platform.OS === 'ios'); // On iOS, the DateTimePicker is not dismissed automatically
     setExamDate(selectedDate || examDate);
@@ -30,21 +41,45 @@ const AddExam = () => {
     setShowTimePicker(true);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async() => {
+    const d = examDate.toISOString().slice(0,10)
+    const t = examTime.toISOString().slice(11,19)
+   
     const formData = {
-      examName,
-      examCode,
-      examDate,
-      examTime,
-      examDuration,
+      exam_name: examName,
+      exam_code: examCode,
+      exam_date: d,
+      exam_time: t,
+      exam_duration: 4,
+      exam_status: false,
+      exam_creator: userData.id,
+      department: userData.department,
+      school: userData.school,
+      course: course,
     };
-
-    console.log('Submitted Data:', formData);
-    // You can perform further actions, such as sending the data to a server, here.
+    console.log('Form Data:', formData);
+    try{
+      const res = await fetch('http://10.0.2.2:8000/api/exam/',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+      const response = await res.json()
+      setAlert({
+        isVisible: true,
+        msg: 'Exam Added Successfully'
+      })
+      console.log('Returned Data:', res);
+    } catch (err) {
+      console.log(err)
+    }
   };
 
   return (
     <View style={styles.container}>
+      { alert.isVisible && <Text>{alert.msg}</Text>}
       <Text>Exam Name</Text>
       <TextInput
         style={styles.input}
@@ -58,7 +93,12 @@ const AddExam = () => {
         onChangeText={(text) => setExamCode(text)}
         value={examCode}
       />
-
+      <Text>Course</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setCourse(text)}
+        value={course}
+      />
       <Text>Exam Date</Text>
       <TouchableOpacity onPress={showDatepicker}>
         <Text>{examDate.toDateString()}</Text>
