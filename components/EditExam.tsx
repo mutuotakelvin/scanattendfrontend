00062185644e8eda02022dbe36b8e74bb83c1ae1
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-28 20:23:09 +0300

    feat(app): edit exam details

---
+++ components/EditExam.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EditExam = () => {
+  return (
+    <View>
+      <Text>EditExam</Text>
+    </View>
+  )
+}
+
+export default EditExam
