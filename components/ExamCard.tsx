a5322dedfb5683af7870165560c805cb4a189b50
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-28 20:19:20 +0300

    feat(app): update exam card

--- components/ExamCard.tsx
+++ components/ExamCard.tsx
@@ -2,10 +2,30 @@ import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 import { Fontisto, Ionicons,FontAwesome5 } from '@expo/vector-icons'
 import { useRouter } from 'expo-router'
+import useAccountStore from '../store/AccountStore'
+import useExamStore from '../store/ExamStore'
 
 
 const ExamCard = ({id,name,date,time,duration,code,qrcode, lec}:any) => {
   const router = useRouter()
+  const {account}: any = useAccountStore()
+  const {setSelectedExam, selectedExam}: any = useExamStore()
+
+  const handleEditExam = () => {
+    const examToEdit = {
+      id,
+      name,
+      date,
+      time,
+      duration,
+      code,
+      qrcode,
+      lec
+    }
+    setSelectedExam(examToEdit)
+    router.push('/exam/edit') 
+    
+  }
 
   return (
     <View className='m-2 p-2 rounded-lg bg-gray-400'>
@@ -30,12 +50,32 @@ const ExamCard = ({id,name,date,time,duration,code,qrcode, lec}:any) => {
           </View>
         </View>
         <View className='mt-3 flex flex-row justify-center items-center space-x-5'>
-          <Pressable className='bg-black rounded-lg w-12 flex flex-1 justify-center items-center' onPress={() => router.push('/exam/attend')}>
+        {
+            account == 'Student' && (
+              <Pressable className='bg-black rounded-lg w-8 flex flex-1 justify-center items-center' onPress={() => router.push('/exam/attend')}>
             <Text className='p-1 text-white'>Attend</Text>
           </Pressable>
-          <Pressable className='bg-black rounded-lg w-12 flex flex-1 justify-center items-center' onPress={() => router.push(`/exam/${id}`)}>
+              
+            )
+          }
+          <Pressable className='bg-black rounded-lg w-8 flex flex-1 justify-center items-center' onPress={() => router.push(`/exam/${id}`)}>
             <Text className='p-1 text-white'>View</Text>
           </Pressable>
+          {
+            account == 'Lecturer' && (
+              <Pressable className='bg-black rounded-lg w-8 flex flex-1 justify-center items-center' onPress={handleEditExam}>
+                <Text className='p-1 text-white'>Edit</Text>
+              </Pressable>
+            )
+          }
+          {
+            account == 'Lecturer' && (
+              <Pressable className='bg-black rounded-lg w-8 flex flex-1 justify-center items-center' onPress={() => router.push('/exam/attend')}>
+                <Text className='p-1 text-white'>Delete</Text>
+              </Pressable>
+              
+            )
+          }
         </View>
       </View>
     </View>
