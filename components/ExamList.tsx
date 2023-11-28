2d52d2605d2930db2df94b1347035eb2c7fcdf42
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-28 20:19:52 +0300

    feat(app): update data from backend

--- components/ExamList.tsx
+++ components/ExamList.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { View, Text, StatusBar } from "react-native";
+import { View, Text, StatusBar, RefreshControl, FlatList } from "react-native";
 import { FlashList } from "@shopify/flash-list";
  
 import exams from "../constants/exams";
 import ExamCard from "./ExamCard";
-const ExamList = () => {
-
+const ExamList = ({data, onRefresh}:any) => {
   return (
-    <FlashList
-      data={exams}
-      renderItem={({ item }) => <ExamCard id={item.id} name={item.exam_name} date={item.exam_date} time={item.exam_time} duration={item.exam_duration} code={item.exam_code} qrcode={item.qr_code} lec={item.exam_creator} />}
-      estimatedItemSize={200}
-    />
+    <View>
+      <FlatList
+        data={data}
+        renderItem={({ item }:any) => <ExamCard id={item.id} name={item.exam_name} date={item.exam_date} time={item.exam_time} duration={item.exam_duration} code={item.exam_code} qrcode={item.qr_code} lec={item.exam_creator} />}
+        keyExtractor={(item:any) => item.id.toString()}
+        refreshControl={
+          <RefreshControl 
+            refreshing={false}
+            onRefresh={onRefresh}
+          />
+        }
+      />
+    </View>
   );
 }
 export default ExamList
