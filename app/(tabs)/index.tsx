4cfaad84f3de5ffcf7ed54686d3b3ff8943d3188
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-11-28 20:17:05 +0300

    feat(app): get data from backend

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,43 @@
 import {StyleSheet, View, Text, Pressable, Dimensions } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'expo-router'
 import Greatings from '../../components/Greatings'
 import ExamList from '../../components/ExamList'
 import MyList from '../../components/ExamList'
 import useAccountStore from '../../store/AccountStore'
+import useExamStore from '../../store/ExamStore'
 
 
 
 const index = () => {
   const router = useRouter()
+  const [data, setData] = useState([])
+  const [refresh, setRefresh] = useState(false)
+  const {setExams}:any = useExamStore()
+
   const {account}:any = useAccountStore()
+
+  const handleRefresh = () => {
+    console.log('refreshing')
+    fetchData()
+    setRefresh(prev => !prev)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    try{
+      const res = await fetch('http://10.0.2.2:8000/api/exam/')
+      const data = await res.json()
+
+      setData(data.results)
+      setExams(data.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <View>
       <View className='mt-6 h-[20%]'>
@@ -29,7 +56,7 @@ const index = () => {
         </View>
       </View>
       <View className='m-1 h-[72%] mt-6 mb-4'>
-        <ExamList />
+        <ExamList data={data} onRrefresh={handleRefresh}/>
       </View>
     </View>
   )
